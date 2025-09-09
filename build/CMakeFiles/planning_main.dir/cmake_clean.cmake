0ef8c7132c2edf8482a36b747639cc5ed23ccd55
jinlong-gft <zhangjinlongh@163.com>
2025-09-09 22:15:09 +0800

    version 0.0.1

---
+++ build/CMakeFiles/planning_main.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/planning_main.dir/planning_main.cpp.obj"
+  "CMakeFiles/planning_main.dir/planning_main.cpp.obj.d"
+  "CMakeFiles/planning_main.dir/pnc_map.cpp.obj"
+  "CMakeFiles/planning_main.dir/pnc_map.cpp.obj.d"
+  "CMakeFiles/planning_main.dir/prosess.cpp.obj"
+  "CMakeFiles/planning_main.dir/prosess.cpp.obj.d"
+  "E:/FlFile/cmake_planning_test/bin/libplanning_main.dll.a"
+  "E:/FlFile/cmake_planning_test/bin/planning_main.exe"
+  "E:/FlFile/cmake_planning_test/bin/planning_main.exe.manifest"
+  "E:/FlFile/cmake_planning_test/bin/planning_main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/planning_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ planning_main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include "prosess.h"
+using std::cout,std::endl;
+
+int main()
+{
+    cout<<"planing start"<<endl;
+    Process pro;
+    pro.planProcess();
+     cout<<"planing end"<<endl;
+     system("pause");
+     return 0;
+
+}
